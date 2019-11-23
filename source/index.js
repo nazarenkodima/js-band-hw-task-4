717a2588a1137790a86a3ca7c47e68933eff0a8e
Dmytro Nazarenko <dmamyk@gmail.com>
2019-11-23 17:40:46 +0200

    chore(initial commit): initial project setup with webpack, commitizen, eslint

---
+++ source/index.js
@@ -0,0 +1,8 @@
+import TransportList from './models/TransportList.js';
+import CostList from './models/CostList.js';
+
+// Styles
+import './theme/init.css';
+
+const transportList = new TransportList();
+const costList = new CostList();
